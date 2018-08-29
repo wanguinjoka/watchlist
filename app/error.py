566f1028e6e3c52d5b3f8f70247cdc13e50e4b86
James Kageni <james.kageni@gmail.com>
2018-08-29 11:03:43 +0300

    added search to views

--- app/error.py
+++ app/error.py
@@ -2,11 +2,11 @@ from flask import render_template
 from app import app
 
 @app.errorhandler(404)
-def four_Ow_four(error):
+def four_ow_four(error):
     """
     Function to render the 404 error page
     :param error:
     :return:
     """
 
-    return render_template(fourowfour.html), 404
+    return render_template('fourowfour.html'), 404
--- app/views.py
+++ app/views.py
@@ -11,13 +11,12 @@ def index():
     View root page function that returns the index page and its data
     """
 
-    #getting popular movies
      # Getting popular movie
     popular_movies = get_movies('popular')
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+    return render_template('index.html', title=title, popular=popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
 # @app.route('/movie/<int:movie_id>')
@@ -37,4 +36,4 @@ def movie(id):
     movie = get_movie(id)
     title = f'{movie.title}'
 
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html', title=title, movie = movie)
